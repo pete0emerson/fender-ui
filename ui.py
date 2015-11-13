72cadfe53b68eafb208d7ec5c3707538b75abfbb
Pete Emerson <pete@automatic.com>
2015-11-13 16:45:06 +0000

    Change port

--- ui.py
+++ ui.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World UI!'
 
 @app.route('/_status/')
 def status():
     return 'OK'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888, debug=True)
+    app.run(host='0.0.0.0', port=8889, debug=True)
