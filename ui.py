01ef76f0a90f845c0dd46b08cf2cb4b14baa311f
Pete Emerson <pete@automatic.com>
2015-11-13 19:11:36 +0000

    Adding dynamic content

--- ui.py
+++ ui.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,5 +12,9 @@ def hello_world():
 def status():
     return 'OK'
 
+@app.route('/display')
+def display():
+    return render_template('display.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8889, debug=True)
+    app.run(host='0.0.0.0', port=8889)
